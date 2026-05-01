b6cfbe578988e28b677b82ec0af4cbdea56dcfcf
Anbin Rubin <rubinanbin.21@gmail.com>
2026-05-01 18:33:33 +0530

    Added new file example2

---
+++ example1.spec.js
@@ -0,0 +1,8 @@
+import {test, expect } from '@playwright/test';
+
+test('Playwright Test1', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.google.com/');
+    console.log(await page.title());
+});
