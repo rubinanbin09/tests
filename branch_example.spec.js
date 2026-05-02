c57ad269a9db0c6ca586884e4d6f1723af850bc6
Anbin Rubin <rubinanbin.21@gmail.com>
2026-05-02 08:54:37 +0530

    A new file branch example was added to learn the branch in github

---
+++ branch_example.spec.js
@@ -0,0 +1,9 @@
+import {test, expect } from '@playwright/test';
+
+test('Playwright Test1', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.google.com/');
+    console.log(await page.title());
+    console.log("This is added for branch example in GitHub");
+});
